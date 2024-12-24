0c6fa456b017906fffb30756813d542cc65111bc
HenriqueSilva42 <167092193+HenriqueSilva42@users.noreply.github.com>
2024-12-23 22:08:09 -0300

    Create EstrategiaPublicacaoArtigo.java

---
+++ modulo5/EstrategiaPublicacaoArtigo.java
@@ -0,0 +1,7 @@
+public class EstrategiaPublicacaoArtigo implements PublicavelInterface {
+    @Override
+    public void publicar() {
+        System.out.println("Publicando o artigo...");
+        // Lógica específica para publicação de artigos (ex: enviar para plataforma de artigos científicos)
+    }
+}
