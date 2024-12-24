79e02e4a28dfb5bddfa000a665a758d99de8108d
HenriqueSilva42 <167092193+HenriqueSilva42@users.noreply.github.com>
2024-12-23 22:07:47 -0300

    Create EstrategiaPublicacaoLivro.java

---
+++ modulo5/EstrategiaPublicacaoLivro.java
@@ -0,0 +1,7 @@
+public class EstrategiaPublicacaoLivro implements PublicavelInterface {
+    @Override
+    public void publicar() {
+        System.out.println("Publicando o livro...");
+        // Lógica específica para publicação de livros (ex: enviar para plataforma de livros)
+    }
+}
